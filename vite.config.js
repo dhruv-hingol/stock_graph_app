290902347ab15281cde8b56681fe210fc94b44e7
dhruv <dhruvhingol2210@gmail.com>
2023-04-20 11:49:56 +0530

    update code

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+const express = require('express');
+const app = express();
+
+app.use(express.static('public', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.jsx')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+  },
+}));
